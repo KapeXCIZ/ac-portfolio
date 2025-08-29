9847ee3bafe44f8c280fbd24701b65fb839e829d
KapeXCIZ <alessio.capecchi.18@gmail.com>
2025-08-29 19:29:38 +0200

    Update styles in SelectSection and globals.css for improved design consistency

--- src/components/SelectSection.tsx
+++ src/components/SelectSection.tsx
@@ -35,41 +35,40 @@ export default function SelectSection() {
     }, []);
 
     return (
-        <section className="flex justify-center flex-col px-4 lg:flex-row items-center text-body h-[90vh] gap-6 container mx-auto py-4 md:py-10 ">
+        <section className="flex justify-center flex-col px-4 lg:flex-row items-center text-body h-[90vh] gap-6 container *:max-h-[1000px] mx-auto py-4 md:py-10 ">
             <a href="/about" onMouseEnter={() => router.prefetch("/about")} onClick={(e) => {
                 setDevAnimating(!devAnimating);
                 handleClick(e);
-            }} className={cn("group cursor-pointer min-w-0 text-left overflow-visible dev-selection card h-full w-full flex flex-col justify-start gap-4 bg-accent-800 text-white/95  flex-[1] transition-[flex] hover:flex-[1.5] rounded-2xl ease-spring duration-(--spring-duration)  relative photography-selection selection  *:z-10", devAnimating ? "animating" : "", safari ? "shadow-xl shadow-accent-400/20 " : "")}>
-                <span className="w-full h-full overflow-hidden p-10">
-                    <h2 className="text-5xl xl:text-7xl font-deco group-hover:tracking-widest transition-all ease-spring duration-(--spring-duration) text-white text-shadow">WEB<br />DEVELOPMENT</h2>
-                    <p className="text-2xl  text-white/0 group-hover:text-shadow transition ease-spring duration-(--spring-duration) group-hover:text-white">Clean code, Sharp pixels and User-first thinking.</p>
+            }} className={cn("group cursor-pointer min-w-0 text-left overflow-visible dev-selection card h-full w-full flex flex-col justify-start gap-4  text-white/95  flex-[1] transition-[flex] hover:flex-[1.5] rounded-2xl ease-spring duration-(--spring-duration)  relative photography-selection selection  *:z-10", devAnimating ? "animating" : "", safari ? "shadow-xl shadow-accent-1000/15 dark:shadow-accent-300/5 " : "")}>
+                <span className="w-full h-full overflow-hidden p-6 md:p-10">
+                    <h2 className="text-4xl sm:text-5xl md:text-6xl xl:text-7xl font-deco group-hover:tracking-widest transition-all ease-spring duration-(--spring-duration) text-white text-shadow text-nowrap">WEBSITES <br className="block sm:hidden lg:block" />AND APPS</h2>
+                    <p className="text-xl leading-tight md:text-2xl  text-white lg:text-white/0 group-hover:text-shadow transition ease-spring duration-(--spring-duration) group-hover:text-white ">Clean code, Sharp pixels and User-first thinking.</p>
                 </span>
-                <span className="absolute w-full h-1/2 backdrop-blur-lg mask-to-t !z-[0] rounded-xl" />
-                <div className="!absolute !-z-30 top-2 inset-0 shadow-color">
+                <span className="absolute w-full h-2/3 lg:h-1/2 backdrop-blur-xl mask-to-t !z-[0] rounded-xl" />
+                <div className="!absolute !-z-30 top-3 inset-0 shadow-color">
                     <Image
                         src="/images/dev-blur.jpg"
                         alt="photo blur"
                         fill
-                        className="object-cover w-full h-full blur-md pointer-events-none -z-30 brightness-120"
+                        className="object-cover w-full h-full blur-md pointer-events-none -z-30 brightness-120 opacity-70"
                     />
                 </div>
             </a>
             <a href="https://photo.alessiocapecchi.com/" onMouseEnter={() => router.prefetch("https://photo.alessiocapecchi.com/")} onClick={(e) => {
                 setPhotoAnimating(!photoAnimating);
                 handleClick(e);
-            }} className={cn("group cursor-pointer min-w-0 text-left overflow-visible card h-full w-full flex flex-col justify-start gap-4 bg-accent-800 text-white/95  flex-[1] transition-[flex] hover:flex-[1.5] rounded-2xl ease-spring duration-(--spring-duration) relative photography-selection selection  *:z-10 ", photoAnimating ? "animating" : "", safari ? "shadow-xl shadow-accent-400/20" : "")}>
-                <span className="w-full h-full overflow-hidden p-10">
-                    <h2 className="text-5xl xl:text-7xl font-deco group-hover:tracking-widest transition-all ease-spring duration-(--spring-duration) text-white text-shadow">PHOTOGRAPY</h2>
-                    <p className="text-2xl  text-white/0 group-hover:text-shadow transition ease-spring duration-(--spring-duration) group-hover:text-white">A visual journey through light, detail, and perspective.</p>
+            }} className={cn("group cursor-pointer min-w-0 text-left overflow-visible card h-full w-full flex flex-col justify-start gap-4  text-white/95  flex-[1] transition-[flex] hover:flex-[1.5] rounded-2xl ease-spring duration-(--spring-duration) relative photography-selection selection  *:z-10 ", photoAnimating ? "animating" : "", safari ? "shadow-xl shadow-accent-1000/15 dark:shadow-accent-300/10" : "")}>
+                <span className="w-full h-full overflow-hidden p-6 md:p-10">
+                    <h2 className="text-4xl sm:text-5xl md:text-6xl xl:text-7xl font-deco group-hover:tracking-widest transition-all ease-spring duration-(--spring-duration) text-white text-shadow">PHOTOGRAPHY</h2>
+                    <p className="text-xl leading-tight md:text-2xl  text-white lg:text-white/0 group-hover:text-shadow transition ease-spring duration-(--spring-duration) group-hover:text-white">A visual journey through light, detail, and perspective.</p>
                 </span >
-                <span className="absolute w-full h-1/2 backdrop-blur-lg mask-to-t !z-[0] rounded-xl" />
-
-                <div className="!absolute !-z-30 top-2 inset-0 shadow-color">
+                <span className="absolute w-full h-2/3 md:h-1/2 backdrop-blur-md mask-to-t !z-[0] rounded-xl" />
+                <div className="!absolute !-z-30 top-3 inset-0 shadow-color">
                     <Image
                         src="/images/foto-blur.jpg"
                         alt="photo blur"
                         fill
-                        className="object-cover w-full h-full blur-md pointer-events-none -z-30 brightness-80 "
+                        className="object-cover w-full h-full blur-md pointer-events-none -z-30 brightness-80  opacity-70"
                     />
                 </div>
             </a>
