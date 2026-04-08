39caf7b05e4f4fda257bbcb09ac202e6ce17ed7c
KapeXCIZ <alessio.capecchi.18@gmail.com>
2026-04-08 22:17:32 +0200

    feat: add projects layout and page components
    - Created a new layout for the projects page with metadata.
    - Implemented the projects page to display project sections with relevant data.
    - Updated global styles for accent colors and glow effects.
    - Removed old home and projects pages to streamline the structure.
    - Refactored components to use new navigation and translation methods.
    - Introduced new contextual link component for locale switching.
    - Enhanced the hero and teaser components with translations.
    - Added a card group component for displaying project images with parallax effects.
    - Updated footer and navbar components to utilize new navigation methods.
    - Implemented middleware for internationalization routing.

--- src/components/AboutTeaser.tsx
+++ src/components/AboutTeaser.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 
+
 import CustomButton from "./CustomButton";
 import { AtIcon, ReadCvLogoIcon } from "@phosphor-icons/react";
 import Image from "next/image";
@@ -15,13 +16,14 @@ export default function AboutTeaser() {
             <span className="absolute w-[1200px] h-[600px] max-w-[96vw]   bg-accent-500/25 object-center rounded-full mask-radial-[50%_50%] mask-radial-from-0%  -z-50" />
             <Image src={"/images/home/siluette.png"} height={200} width={200} alt="Alessio's siluette" className="w-60 mask-b-from-50% mask-b-to-90%  brightness-0 invert dark:invert-0 -top-28 select-none [image-select: none]  absolute " />
             <h1 className="text-center font-sans text-3xl md:text-4xl font-light z-1 px-6">{t("r1")}<br /><span className="italic font-serif">{t("r2")}</span></h1>
+            <h1 className="text-center font-sans text-3xl md:text-4xl font-light z-1 px-6">{t("r1")}<br /><span className="italic font-serif">{t("r2")}</span></h1>
             <div className="flex flex-row gap-3  justify-center items-center">
                 {/* cta */}
                 <NavLink href={"/about"} className="z-0  md:text-lg">
                     <CustomButton className="flex gap-1 items-center justify-center -z-1 " variant="solid"><AtIcon />{t("b1")}</CustomButton>
                 </NavLink>
                 {/* secondary button */}
-                <Link href={"/files/CV.pdf"} target="_blank" className="z-0 md:text-lg">
+                <Link href={"/files/alessio-capecchi-cv.pdf"} target="_blank" className="z-0 md:text-lg">
                     <CustomButton className="flex gap-1 items-center justify-center -z-1 " variant="outline"><ReadCvLogoIcon />CV</CustomButton>
                 </Link>
             </div>
--- src/components/ProjectsHero.tsx
+++ src/components/ProjectsHero.tsx
@@ -9,7 +9,7 @@ export default function ProjectsHero() {
     const t = useTranslations("projects.hero")
 
     return (
-        <div className="w-full flex justify-center items-center mt-10">
+        <div className="w-full px-4 text-center flex justify-center items-center mt-10">
             <h1 className="text-9xl font-deco glow-text text-foreground">{t("h1")}</h1>
         </div>
     )
